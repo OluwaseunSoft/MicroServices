78b9b7ad314defd8f54fc1eb582d9e8f17c86f2b
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-12-29 22:25:40 +0100

    Configuring consumer

--- CommandsService/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using CommandsService.EventProcessing;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace CommandsService.AsyncDataServices
 {
@@ -6,15 +9,65 @@ namespace CommandsService.AsyncDataServices
     {
         private readonly IConfiguration _config;
         private readonly IEventProcessor _eventProcess;
+        private IConnection _connection;
+        private IModel _channel;
+        private string _queueName;
 
         public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
         {
             _config = configuration;
             _eventProcess = eventProcessor;
+            InitializeRabbitMQ();
+        }
+
+        private void InitializeRabbitMQ()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _config["RabbitMQHost"],
+                Port = int.Parse(_config["RabbitMQPort"])
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _queueName = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queue: _queueName, exchange: "trigger",
+            routingKey: "");
+            System.Console.WriteLine("--> Listening on the Message Bus...");
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutDown;
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ModuleHandle, ea) =>
+            {
+                System.Console.WriteLine("--> Event Received!");
+
+                var body = ea.Body;
+                var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+                _eventProcess.ProcessEvent(notificationMessage);
+            };
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true,
+            consumer: consumer);
+            return Task.CompletedTask;
+        }
+
+        private void RabbitMQ_ConnectionShutDown(object sender, ShutdownEventArgs e)
+        {
+            System.Console.WriteLine("--> Connection Shutdown.");
+        }
+
+        public override void Dispose()
+        {
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+            base.Dispose();
         }
     }
 }

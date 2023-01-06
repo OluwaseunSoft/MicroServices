b3bb17de7572431f2ef52c07c47c1f7e777c7659
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-01-06 06:38:56 +0100

    PlatformDataClient

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,12 +1,14 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<ICommandRepo, CommandRepo>();
+builder.Services.AddTransient<IPlatformDataClient, PlatformDataClient>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddControllers();
 builder.Services.AddHostedService<MessageBusSubscriber>();
@@ -16,7 +18,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => 
+builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("InMem"));
 var app = builder.Build();
 
